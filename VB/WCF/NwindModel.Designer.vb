c7ccbf2b1d678f0d8f963cfc6fc89fab26ed583b
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-10-02 15:47:46 +0400

    Change example status to out of maintenance

--- VB/WCF/NwindModel.Designer.vb
+++
@@ -1,7 +0,0 @@
-Imports Microsoft.VisualBasic
-Imports System
-
-' Default code generation is disabled for model 'C:\!DATA\!Visual Studio Projects\WCF\WCF\NwindModel.edmx'. 
-' To enable default code generation, change the value of the 'Code Generation Strategy' designer
-' property to an alternate value. This property is available in the Properties Window when the model is
-' open in the designer.
--- VB/WebApplication/Views/Home/Index.vbhtml
+++
@@ -1,7 +0,0 @@
-﻿@Code
-    ViewBag.Title = "WCF Data Services Sample"
-End Code
-
-<h2>Index</h2>
-
-@Html.Action("GridViewPartial")
